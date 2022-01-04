684145784400cca54633008b26333a5706549e77
krishnakandibanda <krishnakandibanda@gmail.com>
2022-01-04 19:34:28 +0530

    add new class..

---
+++ src/main/java/com/method/Demo.java
@@ -0,0 +1,23 @@
+package com.method;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input = 6;
+		int ou = 8;
+		add(input,ou);
+		Demo cal = new Demo();
+		cal.addtion(input, ou);		
+	}
+	public static int add(int input,int as) {
+		int a = 0,b = 0,c;
+		c=a+b;
+		return c;
+	}
+	public void addtion(int x,int y) {
+		int z=x+y;
+		System.out.println(" Addtion        --    With out return AnyThing (void)   "+z);
+	}
+
+}
