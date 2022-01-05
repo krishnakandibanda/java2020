ce1635ff69c0a8f596e35070f8ea087b5d861108
krishnakandibanda <krishnakandibanda@gmail.com>
2022-01-05 18:23:07 +0530

    "Print String Array using for each loop"

---
+++ src/main/java/com/array/exam/ForeachMethod.java
@@ -0,0 +1,11 @@
+package com.array.exam;
+
+public class ForeachMethod {
+	public static void main(String[] args) {
+		String[] names= {"JAVA","CoreJAVa","Arrays","doWhile","ifElse","For"};
+		System.out.println("Print all String array values\n");
+		for(String name:names) {
+			System.out.println(name);
+		}
+	}
+}
